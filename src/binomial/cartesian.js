7b8cab8a7cc1411f8feeec925fc0de1483571a75
Bartosz <bartek.burgiel@hotmail.com>
2019-10-25 23:17:15 +0200

    Add cartesian class

---
+++ src/binomial/cartesian.js
@@ -0,0 +1,26 @@
+export default class Cartesian {
+    
+    createCartesian = (height, width, xRange, yRange) => {
+        
+        // Assemble coordinate system 
+        drawXAxis(width)
+        drawXAxis(height)
+    } 
+
+    // Setter
+    setXRange = (XRange) => {
+        this.XRange = XRange
+    }
+
+    setYRange = (YRange) => {
+        this.YRange
+    }
+}
+
+const drawXAxis = (width) => {
+
+}
+
+const drawYAxis = (height) => {
+    
+}
