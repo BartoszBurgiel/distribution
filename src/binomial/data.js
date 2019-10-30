f17d7f3b6fb194b21a3ee93025664f2bd5feabb7
Bartosz <bartek.burgiel@hotmail.com>
2019-10-30 21:28:45 +0100

    Complete data class

--- src/binomial/data.js
+++ src/binomial/data.js
@@ -1,25 +1,33 @@
 // Data class will be used to create fields containing variables etc.
 export default class Data {
-    constructor(n, prop, p, x, y, height, width) {
-        this.n = n
-        this.prop = prop
+    constructor(p, x, y, height, width) {
         this.p = p
 
         this.x = x
         this.y = y
+
         this.height = height
         this.width = width
+
+        this.labels = new Map()
     }
 
     display () {
-        this.p.rect(this.y, this.x, this)
+        fill(255);
+        this.p.rect(this.y, this.x, this.width, this.height)
+
+        // Display labels
+        let i = 0
+
+        for (let [key, val] of this.labels) {
+            
+            text(key+'='+val , 20 + this.x, 20 + this.y + (this.height/this.labels.length) )
+            i++
+        }
     }
 
-    update (n, prop) {
-
-        // Update attributes
-        this.n = n 
-        this.prop = prop
+    addLabel(key, val) {
+        this.labels.set(key, val)
     }
 
 }
