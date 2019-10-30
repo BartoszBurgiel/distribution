779ef5481d411c713c801791a53573269d239158
Bartosz <bartek.burgiel@hotmail.com>
2019-10-30 22:45:07 +0100

    Fix equaloty sign warning

--- src/binomial/math.js
+++ src/binomial/math.js
@@ -13,9 +13,9 @@ export default class Distribution {
     
     // get factiorial of a given integer
     factorial = (n) => {
-        if (n == 1) {
+        if (n === 1) {
             return (1)
-        } else if (n == 0) {
+        } else if (n === 0) {
             return (0)
         } else {
             return (n * this.factorial(n - 1))
