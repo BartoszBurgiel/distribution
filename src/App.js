35ff208ff24590b7b03fcecb9110ee24df8286b3
Bartosz <bartek.burgiel@hotmail.com>
2019-10-30 22:05:14 +0100

    Create dir for display and plot purposes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
-import sketch from './binomial/sketch';
+import binomialDistributionGraph from './binomial/graph';
 import './App.css';
 
 const App = () => {
 	return (
 		<>
 			<h1>Binomialverteilung</h1>
-			<P5Wrapper sketch={sketch}></P5Wrapper>
+			<P5Wrapper sketch={binomialDistributionGraph}></P5Wrapper>
 		</>
 	);
 }
