75e14e8c31d5a2199d751b854993024c88dadf1f
Bartosz <bartek.burgiel@hotmail.com>
2019-10-30 22:18:30 +0100

    Remove semicolons

--- src/display/bar.js
+++ src/display/bar.js
@@ -1,15 +1,15 @@
 export default class Bar {
 
     constructor(xPos, yPos, width, height) {
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.width = width;
-        this.height = height;
+        this.xPos = xPos
+        this.yPos = yPos
+        this.width = width
+        this.height = height
     }
 
     display(p) {
-        p.fill(255);
-        p.rect(this.xPos, this.yPos, this.width, -this.height);
-        p.fill(0);
+        p.fill(255)
+        p.rect(this.xPos, this.yPos, this.width, -this.height)
+        p.fill(0)
     }
 }
