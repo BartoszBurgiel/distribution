29ccd46db1cb27607c5caf4d3180fc4811e948a5
Bartosz <bartek.burgiel@hotmail.com>
2019-10-31 14:01:37 +0100

    Restructure Bar class

--- src/display/bar.js
+++ src/display/bar.js
@@ -1,15 +1,20 @@
 export default class Bar {
 
     constructor(xPos, yPos, width, height) {
-        this.xPos = xPos
-        this.yPos = yPos
-        this.width = width
-        this.height = height
+        this.position = {
+            x = xPos,
+            y = yPos 
+        }
+
+        this.dimensions = {
+            width,
+            height
+        }
     }
 
     display(p) {
         p.fill(255)
-        p.rect(this.xPos, this.yPos, this.width, -this.height)
+        p.rect(this.position.x, this.position.y, this.dimensions.width, -this.dimensions.height)
         p.fill(0)
     }
 }
