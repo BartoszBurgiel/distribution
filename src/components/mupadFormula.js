8ea7843a0015ee3c326b2ad28a0a4dfb5e9ad11e
Bartosz Burgiel <bartek.burgiel@hotmail.com>
2019-11-11 23:22:36 +0100

    Complete code display and copying

--- src/components/mupadFormula.js
+++ src/components/mupadFormula.js
@@ -4,33 +4,70 @@ import '../assets/style/mupad.css';
 
 export default class MupadFormula extends React.Component {
 
-    setupCommand = () => {
-        let command = ""
-        let functionName = ""
+    // get function name based on eqType prop
+    getFunctionName = () => {
+        switch(this.props.eqType) {
+            case 'binom':
+                return "P"
+            case 'cBinom':
+                return "F"
+            case 'normal':
+                return "P"
+            default:
+                return "F"    
+        }
+    }
 
+    // Define a function based on eqType
+    defineFunction = () => {
+        
         // determine which function
         switch (this.props.eqType) {
             case 'binom':
-                functionName = "P"
-                command = `${functionName} := stats::binomialPF(${this.props.nVal},${this.props.pVal});\n${functionName}(${this.props.kVal}); \n`
-                break
+
+                return (`${this.getFunctionName()} := stats::binomialPF(${this.props.nVal},${this.props.pVal});\n`)
             case 'cBinom':
-                functionName = "F"
-                command = `${functionName} := stats::binomialCDF(${this.props.nVal},${this.props.pVal});\n${functionName}(${this.props.kVal}); \n`
-                break
+
+                return(`${this.getFunctionName()} := stats::binomialCDF(${this.props.nVal},${this.props.pVal});\n`)
             case 'normal':
-                functionName = "P"
-                command = `${functionName} := stats::normalPF(${this.props.muVal},${this.props.varVal});\n${functionName}(${this.props.kVal}); \n`
-                break
+
+                return(`${this.getFunctionName()} := stats::normalPF(${this.props.muVal},${this.props.varVal});\n`)
             default: 
                 break
         }
+    }
 
+    // Call defined function
+    callFunction = () => {
+        return(`${this.getFunctionName()}(${this.props.kVal});\n`)
+    }
+
+    // Assemble function generating the data for the function
+    assembleData = () => {
         if (this.props.eqType !== 'normal') {
-            command += `\ndaten := [${functionName}(k)$k=0..${this.props.nVal}];\nplot(plot::Bars2d(daten));`
+            return(`daten := [${this.getFunctionName()}(k)$k=0..${this.props.nVal}];\n`)
         } else {
-            command += `\nplotfunc2d(${functionName}(x), x=0..${this.props.nVal}, YRange=0..1);`
+            return ("")
         }
+    }
+
+    // Assemble plotting function
+    plotFunction = () => {
+        if (this.props.eqType !== 'normal') {
+            return(`plot(plot::Bars2d(daten));\n`)
+        } else {
+            return(`plotfunc2d(${this.getFunctionName()}(x), x=0..${this.props.nVal}, YRange=0..1);\n`)
+        }
+    }
+
+
+    setupCommand = () => {
+        let command = ""
+
+        command += this.defineFunction()
+        command += this.callFunction()
+        command += this.assembleData()
+        command += this.plotFunction()
 
         return command
     }
@@ -40,7 +77,16 @@ export default class MupadFormula extends React.Component {
             <>
                 <h2>Mupad - Befehl</h2>
                 <div>
-                    {this.setupCommand()}
+                    <code>
+                        {this.defineFunction()}
+                        <br />
+                        {this.callFunction()}
+                        <br />
+                        {this.assembleData()}
+                        <br />
+                        {this.plotFunction()}
+                        <br />   
+                    </code>
                     <CopyToClipboard text={this.setupCommand()}>
                         <button>Kopieren</button>
                     </CopyToClipboard>
